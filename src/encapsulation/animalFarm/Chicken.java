5bbf9983cc88d30e875bbb429801eb0241377bae
Deni06068 <Deni06068@gmail.com>
2022-07-11 14:11:39 +0300

    Added new task.

---
+++ src/encapsulation/animalFarm/Chicken.java
@@ -0,0 +1,47 @@
+package encapsulation.animalFarm;
+
+public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        if (age < 0 || age > 15){
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+        this.age = age;
+    }
+
+    public double productPerDay(){
+        return calculateProductPerDay();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.", name, age, productPerDay());
+    }
+
+    private double calculateProductPerDay(){
+        double eggsPerDay = 0;
+        if (age < 6){
+            eggsPerDay = 2;
+        }else if (age < 12){
+            eggsPerDay = 1;
+        }else {
+            eggsPerDay = 0.75;
+        }
+
+        return eggsPerDay;
+    }
+}
